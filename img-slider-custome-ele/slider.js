ef5c5872d0fb2e3ecbe4c8f5f92f5be2249516c0
Brennen Bull <brennenbull@gmail.com>
2017-09-26 15:49:03 -0600

    cleaning up some code

--- img-slider-custome-ele/slider.js
+++ img-slider-custome-ele/slider.js
@@ -4,11 +4,9 @@ HTMLElement.prototype.createShadowRoot =
   HTMLElement.prototype.webkitCreateShadowRoot ||
   function() {};
 
-// Add the template to the Shadow DOM
-// Grab our template full of slider markup and styles
 var tmpl = document.querySelector('template');
 
-// Create a prototype for a new element that extends HTMLElement
+
 class ImgSlider extends HTMLElement{
   connectedCallback(){
     var root = this.createShadowRoot();
@@ -17,12 +15,3 @@ class ImgSlider extends HTMLElement{
 }
 
 customElements.define('img-slider', ImgSlider)
-// var ImgSliderProto = Object.create(HTMLElement.prototype);
-
-// Setup our Shadow DOM and clone the template
-// ImgSliderProto.createdCallback = function() {
-//   var root = this.createShadowRoot();
-//   root.appendChild(document.importNode(tmpl.content, true));
-// };
-
-// Register our new element

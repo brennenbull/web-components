2fec6dfe7c63f29497ebcf63a20e647610ae1e50
Brennen Bull <brennenbull@gmail.com>
2017-10-02 16:28:27 -0600

    simple server setup for testing

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = process.env.PORT || 3330;
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join('exp')));
+
+app.listen(port, ()=>{
+  console.log(`app listening on port ${port}`);
+})
+
+module.exports = app;

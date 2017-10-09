e080bc5b6df79e8aafd2c682909610b39af00b33
Brennen Bull <brennenbull@gmail.com>
2017-10-09 12:23:37 -0600

    working webcomp menue

--- public/sticky-nav-bar/tab-list.js
+++ public/sticky-nav-bar/tab-list.js
@@ -5,17 +5,11 @@
       super();
       this.root = this.attachShadow({mode: 'open'});
       this.root.innerHTML = `<style>
-      /*:host{
-        width:170px;
-        float:none;
-        position: relative;
-        display:block;
-        }*/
         :host(:hover){
           width: 100%;
           background-color: #6CCCAA;
           }</style>
-      <li><a href="/#">${this.getAttribute('name')}</a></li>`
+      <li><a href="${this.getAttribute('href')}">${this.getAttribute('name')}</a></li>`
     }
   }
   window.customElements.define('tab-item', TabItem)
@@ -30,12 +24,10 @@
       this.root = this.attachShadow({mode: 'open'});
       this.tmpl = importDoc.querySelector('template');
       this.tmpl.content.querySelector('#lable').innerHTML = this.getAttribute('lable')
-      // this.root.appendChild(document.importNode(this.tmpl.content, true));
+      this.root.appendChild(document.importNode(this.tmpl.content, true));
     }
 
     connectedCallback(){
-      console.log(this.querySelectorAll('tab-item'));
-      this.root.appendChild(document.importNode(this.tmpl.content, true));
     }
   }
   window.customElements.define('tab-list', Tablist);
